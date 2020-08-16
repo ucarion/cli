c38e10a06b44c477406237e0959aa83595b34aee
Ulysse Carion <ulysse@segment.com>
2020-08-16 11:58:51 -0700

    Start doing tree stuff

--- internal/cmdtree/cmdtree.go
+++ internal/cmdtree/cmdtree.go
@@ -1,6 +1,10 @@
 package cmdtree
 
-import "reflect"
+import (
+	"reflect"
+)
+
+const TagSubCommand = "subcmd"
 
 type CommandTree struct {
 	Name     string
@@ -28,10 +32,122 @@ type ChildCommand struct {
 }
 
 func New(funcs []interface{}) (CommandTree, error) {
-	f := reflect.ValueOf(funcs[0])
+	// First, we need to discover all nodes in the graph. We'll construct an
+	// index from config types (which must be unique per func) to the one or
+	// zero funcs that use the config type.
+	//
+	// Next, we'll construct a map of config types to their parent config types.
+	// Each config type has exactly one or zero parent config types.
+	//
+	// From such a map, we'll "reverse" the direction of the relationship to
+	// construct a forest.
+	//
+	// One tricky aspect of this is keeping track of ParentConfigField. It's not
+	// enough to know what the parent config is; we must also keep track of what
+	// index the parent config is in.
 
+	// Construct the initial set of configs.
+	configs := []config{}
+	for _, fn := range funcs {
+		// TODO verify fn is func(context.Context, T) error
+		configs = append(configs, newConfigFromFunc(reflect.ValueOf(fn)))
+	}
+
+	// For each config, see if it has a parent type we don't already have in the
+	// set of configs. If so, add that parent type to the set of configs. Do
+	// this until a pass does not uncover any new configs.
+	newConfigsAdded := true
+	for newConfigsAdded {
+		// Multiple configs may want to add the same parent config type, so
+		// we'll dedupe them with a map.
+		typesToAdd := map[reflect.Type]struct{}{}
+		for _, config := range configs {
+			// Does the config have a parent type at all?
+			if config.ParentType == nil {
+				continue
+			}
+
+			// See if there's an existing config whose Type is our ParentType.
+			ok := false
+			for _, c := range configs {
+				if c.Type == config.ParentType {
+					ok = true
+				}
+			}
+
+			// Our ParentType already is not accounted for. Add it to the set of
+			// types to explore.
+			if !ok {
+				typesToAdd[config.ParentType] = struct{}{}
+			}
+		}
+
+		// For each type to add, construct an instance of config and add it to
+		// the known configs.
+		for t := range typesToAdd {
+			configs = append(configs, newConfigFromType(t))
+		}
+
+		// If we had any types to add, then make sure we do another pass.
+		newConfigsAdded = len(typesToAdd) > 0
+	}
+
+	// With all the edges of our command graph constructed, let's now reverse
+	// the direction of the edges. We'll then start from the "nil" parent type
+	// to construct our tree.
+	children := map[reflect.Type][]config{} // map from parent types to configs
+	for _, c := range configs {
+		children[c.ParentType] = append(children[c.ParentType], c)
+	}
+
+	// We should expect to have exactly one child of the parent type "nil", and
+	// that child is the root command.
+	root := newCmd(children, children[nil][0]) // TODO assert only one root
+
+	return root, nil
+}
+
+type config struct {
+	Func        reflect.Value
+	Type        reflect.Type
+	ParentType  reflect.Type
+	ParentField int
+}
+
+func newConfigFromFunc(fn reflect.Value) config {
+	c := newConfigFromType(fn.Type().In(1))
+	c.Func = fn
+	return c
+}
+
+func newConfigFromType(t reflect.Type) config {
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+
+		if _, ok := f.Tag.Lookup(TagSubCommand); ok {
+			return config{Type: t, ParentType: f.Type, ParentField: i}
+		}
+	}
+
+	return config{Type: t}
+}
+
+func newCmd(children map[reflect.Type][]config, root config) CommandTree {
 	return CommandTree{
-		Config: f.Type().In(1),
-		Func:   f,
-	}, nil
+		Func:     root.Func,
+		Config:   root.Type,
+		Children: newChildCmds(children, root.Type),
+	}
+}
+
+func newChildCmds(children map[reflect.Type][]config, root reflect.Type) []ChildCommand {
+	out := []ChildCommand{}
+	for _, c := range children[root] {
+		out = append(out, ChildCommand{
+			ParentConfigField: c.ParentField,
+			Child:             newCmd(children, c),
+		})
+	}
+
+	return out
 }
--- internal/cmdtree/cmdtree_test.go
+++ internal/cmdtree/cmdtree_test.go
@@ -41,9 +41,62 @@ func TestNew(t *testing.T) {
 		assert.True(t, called)
 		assert.Equal(t, res[0].Interface(), callErr)
 	})
+
+	t.Run("two subcommands", func(t *testing.T) {
+		type root struct{}
+
+		type foo struct {
+			Root root `subcmd:"foo"`
+			XXX  string
+			YYY  string
+		}
+
+		type bar struct {
+			XXX  string
+			YYY  string
+			Root root `subcmd:"foo"`
+		}
+
+		tree, err := cmdtree.New([]interface{}{
+			func(_ context.Context, _ foo) error {
+				return nil
+			},
+			func(_ context.Context, _ bar) error {
+				return nil
+			},
+		})
+
+		assert.NoError(t, err)
+		assert.Equal(t, cmdtree.CommandTree{
+			Config: reflect.TypeOf(root{}),
+			Children: []cmdtree.ChildCommand{
+				cmdtree.ChildCommand{
+					ParentConfigField: 0,
+					Child: cmdtree.CommandTree{
+						Config:   reflect.TypeOf(foo{}),
+						Children: []cmdtree.ChildCommand{},
+					},
+				},
+				cmdtree.ChildCommand{
+					ParentConfigField: 2,
+					Child: cmdtree.CommandTree{
+						Config:   reflect.TypeOf(bar{}),
+						Children: []cmdtree.ChildCommand{},
+					},
+				},
+			},
+		}, stripFuncs(tree))
+	})
 }
 
 func stripFuncs(tree cmdtree.CommandTree) cmdtree.CommandTree {
+	children := []cmdtree.ChildCommand{}
+	for _, c := range tree.Children {
+		c.Child = stripFuncs(c.Child)
+		children = append(children, c)
+	}
+
 	tree.Func = reflect.Value{}
+	tree.Children = children
 	return tree
 }
