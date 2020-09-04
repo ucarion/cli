f1977800be0519ee5cf75171b6198dff5790c98e
Ulysse Carion <ulysse@segment.com>
2020-09-03 20:13:57 -0700

    Add autocompletion module

---
+++ internal/autocompleter/autocompleter.go
@@ -0,0 +1,78 @@
+package autocompleter
+
+import (
+	"reflect"
+	"sort"
+
+	"github.com/ucarion/cli/internal/argparser"
+	"github.com/ucarion/cli/internal/cmdtree"
+	"github.com/ucarion/cli/internal/command"
+)
+
+func Autocomplete(tree cmdtree.CommandTree, args []string) []string {
+	parser := argparser.New(tree)
+
+	for _, arg := range args {
+		if err := parser.ParseArg(arg); err != nil {
+			// An autocompleter can't usefully return an error. If we can't
+			// parse args successfully, we just won't return any suggestions.
+			return nil
+		}
+	}
+
+	if parser.Flag.FieldIndex != nil {
+		// We are expecting a flag's value next. If that flag has an
+		// autocomplete func, we'll return that func's results. Otherwise, we
+		// have no suggestions.
+		if !parser.Flag.AutocompleteFunc.IsValid() {
+			return nil
+		}
+
+		out := parser.Flag.AutocompleteFunc.Call([]reflect.Value{parser.Config})
+		return out[0].Interface().([]string)
+	}
+
+	var out []string
+
+	// As long as flags aren't terminated, then the next argument could be a
+	// flag.
+	if !parser.FlagsTerminated {
+		for _, f := range parser.CommandTree.Flags {
+			if f.LongName != "" {
+				out = append(out, "--"+f.LongName)
+			} else {
+				out = append(out, "-"+f.ShortName)
+			}
+		}
+	}
+
+	// If the flag has children commands, then suggest those children command
+	// names.
+	if parser.CommandTree.Children != nil {
+		for childCmd := range parser.CommandTree.Children {
+			out = append(out, childCmd)
+		}
+
+		// The rest of the possible suggestions are for posargs, which we cannot
+		// have because we instead have child commands.
+		sort.Strings(out)
+		return out
+	}
+
+	var posArg command.PosArg
+	if parser.PosArgIndex == len(parser.CommandTree.PosArgs) {
+		posArg = parser.CommandTree.Trailing
+	} else if parser.CommandTree.PosArgs != nil {
+		posArg = parser.CommandTree.PosArgs[parser.PosArgIndex]
+	}
+
+	if posArg.FieldIndex != nil {
+		if posArg.AutocompleteFunc.IsValid() {
+			fnOut := posArg.AutocompleteFunc.Call([]reflect.Value{parser.Config})
+			out = append(out, fnOut[0].Interface().([]string)...)
+		}
+	}
+
+	sort.Strings(out)
+	return out
+}
---
+++ internal/autocompleter/autocompleter_test.go
@@ -0,0 +1,139 @@
+package autocompleter_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/ucarion/cli/internal/autocompleter"
+	"github.com/ucarion/cli/internal/cmdtree"
+)
+
+func TestAutocomplete_Basic(t *testing.T) {
+	type args struct{}
+
+	tree, err := cmdtree.New([]interface{}{
+		func(_ context.Context, _ args) error { return nil },
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"--help"}, autocompleter.Autocomplete(tree, nil))
+}
+
+func TestAutocomplete_Flags(t *testing.T) {
+	type args struct {
+		A string `cli:"-a"`
+		B string `cli:"--bravo"`
+		C int    `cli:"--charlie,-c"`
+	}
+
+	tree, err := cmdtree.New([]interface{}{
+		func(_ context.Context, _ args) error { return nil },
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t,
+		[]string{"--bravo", "--charlie", "--help", "-a"},
+		autocompleter.Autocomplete(tree, nil))
+
+	// This is an invalid invocation. You can't set C (an int) to "xxx".
+	assert.Equal(t, []string(nil), autocompleter.Autocomplete(tree, []string{"cmd", "-cxxx"}))
+}
+
+type autocompleteArgs struct {
+	A string   `cli:"-a"`
+	B string   `cli:"-b"`
+	C string   `cli:"c"`
+	D string   `cli:"d"`
+	E []string `cli:"...e"`
+}
+
+func (a autocompleteArgs) Autocomplete_B() []string {
+	return []string{a.A}
+}
+
+func (a autocompleteArgs) Autocomplete_C() []string {
+	return []string{a.A}
+}
+
+func (a autocompleteArgs) Autocomplete_E() []string {
+	return []string{"xxx", "yyy"}
+}
+
+func TestAutocomplete_FlagAutocomplete(t *testing.T) {
+	tree, err := cmdtree.New([]interface{}{
+		func(_ context.Context, _ autocompleteArgs) error { return nil },
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t,
+		[]string{"xxx"},
+		autocompleter.Autocomplete(tree, []string{"cmd", "-axxx", "-b"}))
+	assert.Equal(t,
+		[]string(nil),
+		autocompleter.Autocomplete(tree, []string{"cmd", "-a"}))
+}
+
+func TestAutocomplete_PosArgAutocomplete(t *testing.T) {
+	tree, err := cmdtree.New([]interface{}{
+		func(_ context.Context, _ autocompleteArgs) error { return nil },
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t,
+		[]string{"--help", "-a", "-b", "xxx"},
+		autocompleter.Autocomplete(tree, []string{"cmd", "-axxx"}))
+}
+
+func TestAutocomplete_TrailingAutocomplete(t *testing.T) {
+	tree, err := cmdtree.New([]interface{}{
+		func(_ context.Context, _ autocompleteArgs) error { return nil },
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t,
+		[]string{"--help", "-a", "-b", "xxx", "yyy"},
+		autocompleter.Autocomplete(tree, []string{"cmd", "a", "b"}))
+}
+
+type rootArgs struct {
+	X string `cli:"-x"`
+	Y string `cli:"-y"`
+	Z string `cli:"-z"`
+}
+
+type sub1Args struct {
+	Root rootArgs `cli:"sub1,subcmd"`
+	A    string   `cli:"-a"`
+}
+
+func (_ sub1Args) Autocomplete_A() []string {
+	return []string{"xxx"}
+}
+
+type sub2Args struct {
+	Root rootArgs `cli:"sub2,subcmd"`
+	B    string   `cli:"-b"`
+}
+
+func (_ sub2Args) Autocomplete_B() []string {
+	return []string{"yyy"}
+}
+
+func TestAutocomplete_ExecutableWithSubcommands(t *testing.T) {
+	tree, err := cmdtree.New([]interface{}{
+		func(_ context.Context, _ sub1Args) error { return nil },
+		func(_ context.Context, _ sub2Args) error { return nil },
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t,
+		[]string{"--help", "-x", "-y", "-z", "sub1", "sub2"},
+		autocompleter.Autocomplete(tree, []string{"cmd"}))
+	assert.Equal(t,
+		[]string{"--help", "-a"},
+		autocompleter.Autocomplete(tree, []string{"cmd", "sub1"}))
+	assert.Equal(t,
+		[]string{"--help", "-b"},
+		autocompleter.Autocomplete(tree, []string{"cmd", "sub2"}))
+}
